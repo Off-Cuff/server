0551d20eae8500370c22bc374c4e4384f8b63e14
Austin <joha0033@gmail.com>
2017-09-20 16:19:54 -0600

    herok

--- seeds/01_host.js
+++ seeds/01_host.js
@@ -4,7 +4,7 @@ const saltRounds = 10;
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.raw('TRUNCATE host CASCADE; ALTER SEQUENCE host_id_seq restart with 1')
+  return knex.raw('TRUNCATE host CASCADE')
     .then(function () {
       // Inserts seed entries
       const salt = bcrypt.genSaltSync(saltRounds);
